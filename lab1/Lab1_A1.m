2022858e1c0386786085498b33ce7abe3b3cb074
GlacialStrata <colinmc1900@gmail.com>
2025-09-17 16:09:22 -0400

    New Format :D

---
+++ lab1/Lab1_A1.m
@@ -0,0 +1,18 @@
+f = @(t) exp(-t).*cos(2*pi*t);
+t = (-2:2); 
+f(t)
+subplot(2,1,1)
+plot(t,f(t));
+title('Problem A.1: Figure 1.46')
+xlabel('t'); 
+ylabel('f(t)'); grid;
+
+g = @(a) exp(-a).*cos(2*pi*a);
+a = (-2:0.01:2); 
+g(a)
+subplot(2,1,2)
+plot(a,g(a));
+title('Problem A.1: Figure 1.47')
+xlabel('a'); 
+ylabel('g(a)'); grid;
+
