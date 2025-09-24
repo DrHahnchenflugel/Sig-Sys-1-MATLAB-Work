ff6dc811fd018da825e147d1db66c146e7a26afa
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2025-09-24 16:03:54 -0400

    Added legends for A1..3

--- lab1/Lab1_A1.m
+++ lab1/Lab1_A1.m
@@ -1,18 +1,19 @@
 f = @(t) exp(-t).*cos(2.*pi.*t);
-t = (-2:2); 
+t = (-2:2);
 f(t);
 subplot(2,1,1)
 plot(t,f(t));
 title('Lab 1 A.1: Figure 1.46')
-xlabel('t'); 
+xlabel('t');
 ylabel('f(t)'); grid;
+legend('f(t)')
 
 g = @(a) exp(-a).*cos(2.*pi.*a);
-a = (-2:0.01:2); 
+a = (-2:0.01:2);
 g(a);
 subplot(2,1,2)
 plot(a,g(a));
 title('Lab 1 A.1: Figure 1.47')
-xlabel('a'); 
+xlabel('a');
 ylabel('g(a)'); grid;
 
--- lab1/Lab1_A2.m
+++ lab1/Lab1_A2.m
@@ -6,3 +6,4 @@ plot(t,f(t), '*-r');
 title('Lab 1 A.2: Function e^-t for 5 points [-2,-1,0,1,2]')
 xlabel('t');
 ylabel('f(t)'); grid;
+legend('f(t)')
