c77a922599e8e9126f89b2e4ef3d4c1f4849839b
GlacialStrata <colinmc1900@gmail.com>
2025-11-04 14:39:17 -0500

    More code

--- lab3/Lab3_A3_x1.m
+++ lab3/Lab3_A3_x1.m
@@ -3,19 +3,8 @@ clc;   %Clear command window
 clf;   %Clear figure window
 clear; %Clear variables from RAM
 
-function [D] = Dn(d,n)
-
-D1 = [0.5,0,-0.5*1i,0,0.5*1i,0,0.5];
-D2 = (1/(n.*pi)*sin((n*pi)/2));
-D3 = (1/(n.*pi)*sin((n*pi)/4));
-
-if (d==1)
-    D=D1;
-end
-if (d==2)
-    D=D2;
-end
-if (d==3)
-    D=D3;
-end
-end
+Dn1(-1)
+Dn1(1)
+Dn1(-3)
+Dn1(3)
+Dn1(0)
